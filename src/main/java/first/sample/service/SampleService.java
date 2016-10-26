0559ebdc062b797498e490398c6c88c5f0d54aee
jongbochung <jongbo.chung@gmail.com>
2016-10-27 09:12:32 +1100

    2nd commit

--- src/main/java/first/sample/service/SampleService.java
+++ src/main/java/first/sample/service/SampleService.java
@@ -8,8 +8,13 @@ import javax.servlet.http.HttpServletRequest;
 public interface SampleService {
 
 	List<Map<String, Object>> selectBoardList(Map<String, Object> map) throws Exception;
+
 	void insertBoard(Map<String, Object> map, HttpServletRequest request) throws Exception;
+
 	Map<String, Object> selectBoardDetail(Map<String, Object> map) throws Exception;
-	void updateBoard(Map<String, Object> map) throws Exception;
+
+	void updateBoard(Map<String, Object> map, HttpServletRequest request) throws Exception;
+
 	void deleteBoard(Map<String, Object> map) throws Exception;
+
 }
